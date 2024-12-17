1a005a2ceeb3a371805ce658f75930c0a65de9dd
daniele.gattuso <d.gattuso09@gmail.com>
2024-12-17 16:59:34 +0100

    start

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+
+// Chiediamo alla persona quanti km vuole percorrere
+let kmUser = prompt("Quanti km vuoi percorrere?");
+
+// Chiediamo l'età della persona
+let ageUser = prompt("Quanti anni hai??");
